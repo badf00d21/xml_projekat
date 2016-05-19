f6881116e8f8fecfba59dfa152a601015383d51e
vladimir Djurdjevic <vladimirdjurdjevic93@gmail.com>
2016-05-20 01:57:37 +0200

    Configured to consume JWT tokens issued by Parliament.AuthServer

---
+++ Parliament/Parliament.Api.SGNS/Endpoints/AuthTestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace Parliament.Api.SGNS.Endpoints
+{
+    [Authorize]
+    public class AuthTestController : ApiController
+    {
+        [HttpGet]
+        [Route("api/test/token")]
+        public async Task<IHttpActionResult> TokenTest()
+        {
+            return Ok("Radi");
+        }
+    }
+}
