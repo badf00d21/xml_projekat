bab5c52d5173b3ee38e27266627e76a103bb2376
Marko Micic <chakmi1@gmail.com>
2016-06-12 13:42:19 +0200

    Db connection string

--- Parliament/Parliament.AuthServer/Database/AuthDbContext.cs
+++ Parliament/Parliament.AuthServer/Database/AuthDbContext.cs
@@ -7,11 +7,12 @@ using System.Web;
 namespace Parliament.AuthServer.Database
 {
 	//"Server=W7ENT\\OASYSHDB;Database=ParliamentDb;Integrated Security=True;"
+    //Server=MARKO-PC\\IME_MOJE;Database=AuthDb;Integrated Security = True;
 
     public class AuthDbContext : DbContext
     {
         public DbSet<AuthClient> AuthClients { get; set; }
 
-		public AuthDbContext() : base("AuthDb") { }
+		public AuthDbContext() : base("Server=MARKO-PC\\IME_MOJE;Database=AuthDb;Integrated Security=True;") { }
     }
 }
