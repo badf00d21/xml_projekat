cd3f92490c76bfc17acb18b709e71ee9e2898f9a
badf00d21 <jiricekova31@gmail.com>
2016-06-14 15:21:17 +0200

    asd

---
+++ Parliament/Parliament.AngularClient/app/controllers/newActController.js
@@ -0,0 +1,22 @@
+﻿(function (angular) {
+    "use strict";
+
+    angular.module('app.NewActCtrl', [])
+        .controller('newActController', function ($scope, $location, Auth) {
+
+          /*  Auth.isLogged(function (isLogged) {
+                if (!isLogged) {
+                    $location.path('/login');
+                }
+            });*/
+
+            (function () {
+                $scope.options = {
+                    schemaUri: '/api/schemas/',
+                    schemaName: 'act.xsd',
+                    rootElement: 'act',
+                    submitPath: '/api/acts'
+                }
+            }());
+        });
+}(angular));
