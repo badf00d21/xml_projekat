cefa5b5fef535151fb5d16898f3df731c5fe2cd5
vladimirdjurdjevic <vladimirdjurdjevic93@gmail.com>
2016-06-14 18:09:08 +0200

    Propose act enpoint done

--- Parliament/Parliament.Api.SGNS/Endpoints/DocumentController.cs
+++ Parliament/Parliament.Api.SGNS/Endpoints/DocumentController.cs
@@ -33,31 +33,39 @@ namespace Parliament.Api.SGNS.Endpoints
 				getActQuery.SetNewStringVariable("datum_vreme_predlaganja", document.DocumentElement.Attributes["parliament:DatumVremePredlaganja"].Value);
 				getActQuery.SetNewStringVariable("serijski_broj",  document.DocumentElement.Attributes["SerijskiBroj"].Value);
 
+				getActQuery.SetNewStringVariable("text", "");
+				getActQuery.SetNewStringVariable("datum_vreme_usvajanja", "");
+				getActQuery.SetNewStringVariable("naslov_propisa", "");
+				getActQuery.SetNewStringVariable("status", "");
+
 				ResultSequence getActQueryResult = session.SubmitRequest(getActQuery);
 				var xmlResult = new XmlDocument();
 				xmlResult.LoadXml(getActQueryResult.AsString());
 
+				
 				if (xmlResult.SelectSingleNode("Propisi").HasChildNodes)
 					return BadRequest("Xml document with same date and serial number already exists!");
 
 				X509Store store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
 				store.Open(OpenFlags.ReadOnly);
 
-				var certificate = store.Certificates.Find(X509FindType.FindBySubjectName, "vladimirdjurdjevic93@gmail.com", true)[0];
+				var certificates = store.Certificates.Find(X509FindType.FindBySubjectName, "vladimirdjurdjevic93@gmail.com", false);
+				var targetCertificate = certificates.Count > 0 ? certificates[0] : null;
+
 				store.Close();
 
-				if (certificate == null)
+				if (targetCertificate == null)
 					return BadRequest("Could not find users certificate!");
 				
-				XMLUtils.SignXmlDocument(document, certificate);
+				XMLUtils.SignXmlDocument(document, targetCertificate);
 
-				var addActQuery = session.NewModuleInvoke("AddActQuery.xqy");
-				addActQuery.SetNewStringVariable("act_string", document.ToString());
+				var addActQuery = session.NewModuleInvoke("/AddActQuery.xqy");
+				addActQuery.SetNewStringVariable("act_string", document.InnerXml);
 
 				ResultSequence addActQueryResult = session.SubmitRequest(addActQuery);
 
-				if (addActQueryResult.AsString() == "NOT OK")
-					return BadRequest("Invalid XML");
+				if (addActQueryResult.AsString() != "OK")
+					return BadRequest(addActQueryResult.AsString());
 
 				return Created(addActQueryResult.AsString(), doc);
 			}
